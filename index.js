51be128ca2413b97a924a634ec691abac765e953
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-03 02:30:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your functions and code here
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+
+function appendKitten(name){
+  
+  var newKittens = [...kittens, name]
+  return newKittens
+}
+
+function 
+
